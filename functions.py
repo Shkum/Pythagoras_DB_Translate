2044bce15f521847915376919b9721f8a0fcb8b4
Error <shkum79@ukr.net>
2022-11-13 15:29:35 +0100

    Update

--- functions.py
+++ functions.py
@@ -1,15 +1,15 @@
 def split_text(text, split_len):
     result = []
     start = 0
-    end = 0
     txt: str = text[start:start + split_len]
-
     while len(txt):
-
-
         end = txt.rfind('.', start, start + split_len) + 1
+        end = start + split_len + 1 if end == 0 else end
         txt = txt[:end]
         result.append(txt)
         text = text[end:]
         txt: str = text[start:start + split_len]
     return result
+
+
+
